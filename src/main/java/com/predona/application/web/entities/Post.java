38a6c1cb52659d35cc0b72504fb85c65e156e2fa
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-15 17:55:27 +0300

    Domain Object Post.java

---
+++ src/main/java/com/predona/application/web/entities/Post.java
@@ -0,0 +1,56 @@
+package com.predona.application.web.entities;
+
+import org.jooq.Comment;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private Integer id;
+    private String title;
+    private String content;
+    private Timestamp createdOn;
+    private List<Comment> comments = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Timestamp createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
