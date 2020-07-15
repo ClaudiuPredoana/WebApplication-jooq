b367b0976344585c049d593b2249e1dfa95ba178
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-15 18:21:18 +0300

    create two utility methods that extract post and comment information from the Record object

--- src/main/java/com/predona/application/web/services/PostService.java
+++ src/main/java/com/predona/application/web/services/PostService.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.predona.application.web.entities.Comment;
 import com.predona.application.web.entities.Post;
-import static com.predona.application.web.entities.Post.POSTS;
-import static com.predona.application.web.entities.Comment.COMMENTS;
+import static com.predona.application.web.entities.Post.*;
+import static com.predona.application.web.entities.Comment.*;
 
 
 @Service
@@ -19,4 +19,25 @@ public class PostService {
 
     @Autowired
     private DSLContext dsl;
+
+    private Post getPostEntity(Record r) {
+        Integer id = r.getValue(POSTS.ID, Integer.class);
+        String title = r.getValue(POSTS.TITLE, String.class);
+        String content = r.getValue(POSTS.CONTENT, String.class);
+    Timestamp createdOn = r.getValue(POSTS.CREATED_ON, Timestamp.class);
+    return new Post(id, title, content, createdOn);
+    }
+
+    private Comment getCommentEntity(Record r) {
+        Integer id = r.getValue(COMMENTS.ID, Integer.class);
+        Integer postId = r.getValue(COMMENTS.POST_ID, Integer.class);
+        String name = r.getValue(COMMENTS.NAME, String.class);
+    }
+
+    String email = r.getValue(COMMENTS.EMAIL, String.class);
+    String content = r.getValue(COMMENTS.CONTENT, String.class);
+    Timestamp createdOn = r.getValue(COMMENTS.CREATED_ON, Timestamp.class);
+    return new Comment(id, postId, name, email, content, createdOn);
 }
+
+
